40389834c1eb6f8ebfa1c44a5ed379cf05ef2507
milena-t <milena.trabert@t-online.de>
2026-03-25 14:34:34 +0100

    picard mark duplicates

--- salmon_mapping/star_picard_deduplicate.sh
+++ salmon_mapping/star_picard_deduplicate.sh
@@ -5,7 +5,6 @@
 #SBATCH -t 4:00:00
 #SBATCH -J picard_deduplicate
 #SBATCH -o picard_deduplicate.out
-#SBATCH --array=1-72
 
 # Load modules
 module load picard/3.4.0-Java-17
@@ -17,24 +16,29 @@ OUTPUT_DIR="$INPUT_DIR/picard_marked_indexed"
 
 mkdir -p "$OUTPUT_DIR"
 
-#Make an array of all the .bam files 
-BAM_FILES=("$INPUT_DIR"/*_Aligned.sortedByCoord.out.bam)
-
-#pick out the specific bam file for each array job 
-BAM="${BAM_FILES[$SLURM_ARRAY_TASK_ID - 1]}"  #arrays are 0-based
-
-SAMPLE=$(basename "$BAM" "_Aligned.sortedByCoord.out.bam")
-OUTPUT_BAM="$OUTPUT_DIR/${SAMPLE}_marked_duplicates.bam"
-METRICS="$OUTPUT_DIR/${SAMPLE}_markdup_metrics.txt"
-
 echo "Array task $SLURM_ARRAY_TASK_ID processing $SAMPLE..."
 
-# Mark duplicates
-java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
-    -INPUT $BAM \
-    -OUTPUT $OUTPUT_BAM \
-    -METRICS_FILE $METRICS \
-    -VALIDATION_STRINGENCY LENIENT
+# Loop over samples
+for BAM in "$INPUT_DIR"/*_Aligned.sortedByCoord.out.bam; do
+    SAMPLE=$(basename "$BAM" "_Aligned.sortedByCoord.out.bam")
+    OUTPUT_BAM="$OUTPUT_DIR/${SAMPLE}_marked_duplicates.bam"
+    METRICS="$OUTPUT_DIR/${SAMPLE}_markdup_metrics.txt"
 
-# Index the marked BAM
-samtools index "$OUTPUT_BAM"
+    echo " "
+    echo " ======================= marking duplicates sample: $SAMPLE ======================= "
+
+    # Mark duplicates
+    java -jar $EBROOTPICARD/picard.jar MarkDuplicates \
+        -INPUT $BAM \
+        -OUTPUT $OUTPUT_BAM \
+        -METRICS_FILE $METRICS \
+        -VALIDATION_STRINGENCY LENIENT
+
+    # Index the marked BAM
+    samtools index "$OUTPUT_BAM"
+
+done
+
+echo " "
+echo "All samples mapped successfully!"
+
