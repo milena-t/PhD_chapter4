3e57447a2039694059c817a20bcfb5b01099e1b2
milena-t <milena.trabert@t-online.de>
2026-04-28 14:00:30 +0200

    update LFC scatter with m/f shared gene highlights

--- src/plot_DE_stuff.py
+++ src/plot_DE_stuff.py
@@ -276,7 +276,10 @@ def plot_sig_LFC_overlap(tables_dict:dict, p_sig = 0.05, min_LFC = 0, LFC_filena
     for cat in [table_a,table_b,"shared"]:
         ax.scatter(1000,1000,color = colors_dict[cat], s=ps, alpha = 0.75, label = cat)
     if len(excl_geneIDs)>0:
-        ax.scatter(1000,1000,color = "#666666", s=ps*1.5, alpha = 1, label = "also DE\nin females", marker="1")
+        shared_lab = "also DE\nin females"
+        if "females" in LFC_filename:
+            shared_lab = "also DE\nin males"
+        ax.scatter(1000,1000,color = "#666666", s=ps*1.5, alpha = 1, label = shared_lab, marker="1")
     
     ax.set_ylim([min_yline,max_yline])
     ax.set_xlim([min_xline,max_xline])
@@ -301,6 +304,8 @@ if __name__ == "__main__":
         "day14" : ['gene-428738', 'gene-224697', 'gene-222350', 'gene-428765', 'gene-222600', 'gene-224875', 'gene-241001', 'gene-430032', 'gene-220028', 'gene-222486', 'gene-241055', 'gene-224357', 'gene-226245', 'gene-225738', 'gene-224968', 'gene-222531', 'gene-430263', 'gene-224201', 'gene-225107', 'gene-225236', 'gene-225140', 'gene-224227', 'gene-390616', 'gene-225709', 'gene-225325', 'gene-222332', 'gene-222519', 'gene-430314', 'gene-120952', 'gene-240871', 'gene-224860', 'gene-326873', 'gene-240929', 'gene-80359', 'gene-84970', 'gene-322912', 'gene-326849', 'gene-81427', 'gene-323148', 'gene-322927', 'gene-224782', 'gene-218529', 'gene-224743', 'gene-240623', 'gene-222383', 'gene-225173', 'gene-222365', 'gene-222344', 'gene-237881', 'gene-430068', 'gene-224956', 'gene-225720', 'gene-224682', 'gene-431701', 'gene-222555', 'gene-224896', 'gene-403809', 'gene-240910', 'gene-323803', 'gene-390956', 'gene-430080', 'gene-225635', 'gene-240833', 'gene-224593', 'gene-241126', 'gene-225030', 'gene-240691', 'gene-391222', 'gene-90157'],
         "day16" : ['gene-224697', 'gene-222600', 'gene-224875', 'gene-241001', 'gene-220028', 'gene-222486', 'gene-224357', 'gene-224968', 'gene-222159', 'gene-323148', 'gene-223773', 'gene-224782', 'gene-240623', 'gene-225173', 'gene-222344', 'gene-225720', 'gene-431701', 'gene-222555', 'gene-323803', 'gene-225635', 'gene-430080', 'gene-87700', 'gene-330102', 'gene-225030', 'gene-223419', 'gene-90157', 'gene-241262', 'gene-428738', 'gene-222350', 'gene-428765', 'gene-224079', 'gene-225325', 'gene-222332', 'gene-430314', 'gene-120952', 'gene-223491', 'gene-84970', 'gene-322927', 'gene-237881', 'gene-430068', 'gene-224956', 'gene-224682', 'gene-224896', 'g14784', 'gene-240833', 'gene-240691', 'gene-286545', 'gene-223318', 'gene-124877', 'gene-225738', 'gene-222531', 'gene-430263', 'gene-407280', 'gene-225140', 'gene-224227', 'gene-225709', 'gene-224890', 'gene-80359', 'gene-322912', 'gene-227370', 'gene-224743', 'gene-406796', 'gene-240910', 'gene-390956', 'gene-391222', 'gene-430032', 'gene-229506', 'gene-241055', 'gene-226245', 'gene-225107', 'gene-224201', 'gene-225236', 'gene-390616', 'gene-282853', 'gene-222519', 'gene-240871', 'gene-224860', 'gene-326873', 'gene-240929', 'gene-326849', 'gene-222383', 'gene-222365', 'gene-403809', 'gene-224593', 'gene-241126', 'gene-222746', 'gene-238407'],
         "day18" : ['gene-428738', 'gene-224697', 'gene-224875', 'gene-241055', 'gene-223758', 'gene-430263', 'gene-225236', 'gene-225325', 'gene-301479', 'gene-120952', 'gene-223491', 'gene-224860', 'gene-223773', 'gene-240623', 'gene-406796', 'gene-225720', 'gene-238849', 'gene-227308', 'gene-240833', 'gene-224593', 'gene-240691'],
+        "SL1" : [],
+        "SL3" : ['gene-99775', 'gene-40274', 'gene-304827', 'gene-92346', 'gene-306335', 'gene-285669', 'gene-120763', 'gene-2286', 'gene-97407', 'gene-232392', 'gene-328941', 'gene-166511', 'gene-39692', 'gene-384091', 'gene-74686', 'gene-122220', 'gene-218723', 'gene-414353', 'gene-312890', 'gene-153482', 'gene-39770', 'gene-132340', 'gene-253632', 'gene-378608', 'gene-206556', 'gene-336703', 'gene-21229', 'gene-166391', 'gene-120784', 'gene-87502', 'gene-317372', 'gene-73253', 'gene-211196', 'gene-9548', 'gene-60190', 'gene-234650', 'gene-410057', 'gene-121262', 'gene-100036', 'gene-227137', 'gene-75744', 'gene-279912', 'gene-343203', 'gene-233901', 'gene-163028', 'gene-238407', 'gene-39680', 'gene-198700', 'gene-231228', 'gene-410209', 'gene-143368', 'gene-388261', 'gene-30328', 'gene-226944', 'gene-334263', 'gene-130081', 'gene-47823', 'gene-228519', 'gene-350792', 'gene-277340', 'gene-182683', 'gene-60151', 'gene-206576', 'gene-202718', 'gene-130096', 'gene-377275', 'gene-244780', 'gene-288834', 'gene-189246', 'gene-48535', 'gene-205011', 'gene-253157', 'gene-62891', 'gene-69698']
     }
 
     ############################################
@@ -359,9 +364,9 @@ if __name__ == "__main__":
                     venn_title = f"sig. DE genes overlap ({category})\n{venn_cat}"
                     plot_venn_DE_genes(venn_paths_dict, venn_filename=venn_filename, venn_title=venn_title)
 
-    ## compare if the same genes are DE between lines in males as in females
+    ## compare if the same genes are DE between lines within days in males as in females
     if False:
-        venn_sets = {
+        venn_sets_day = {
             "sex_separated" : {
                 "day14" : {
                     "females" : ["SL1_14 - SL3_14"],
@@ -374,10 +379,11 @@ if __name__ == "__main__":
                 "day18" : {
                     "females" : ["SL1_18 - SL3_18"],
                     "males" : ["SL1_18 - SL3_18"]
-                },
+                }
             }
         }
-        for separation, days_dict in venn_sets.items():
+        
+        for separation, days_dict in venn_sets_day.items():
             print(f"\n=========================== {separation} ===========================")
             for day, sexes_contrasts_dict in days_dict.items():
                 print(f"\n ------------------- {day} -------------------")
@@ -393,6 +399,36 @@ if __name__ == "__main__":
                 venn_title = f"sig. DE genes overlap ({day_})\nfemales and males"
                 shared_list = plot_venn_DE_genes(venn_paths_dict, venn_filename=venn_filename, venn_title=venn_title, get_shared_list=True)
                 print(shared_list)
+    
+    ## compare if the same genes are DE between days within lines in males as in females
+    if False:
+        venn_sets_line = {
+            "sex_separated" : {
+                "SL1" : {
+                    "females" : ["SL1_18 - (SL1_14+SL1_16)/2"],
+                    "males" : ["SL1_18 - (SL1_14+SL1_16)/2"]
+                },
+                "SL3" : {
+                    "females" : ["SL3_18 - (SL3_14+SL3_16)/2"],
+                    "males" : ["SL3_18 - (SL3_14+SL3_16)/2"]
+                },
+            }
+        }
+        for separation, days_dict in venn_sets_line.items():
+            print(f"\n=========================== {separation} ===========================")
+            for line, sexes_contrasts_dict in days_dict.items():
+                print(f"\n ------------------- {line} -------------------")
+
+                venn_paths_dict = {}
+                for sex, venn_contrasts_list in sexes_contrasts_dict.items():
+                    print(f"{sex} : {venn_contrasts_list}")
+
+                    venn_paths_dict[sex] = table_paths[separation][sex][venn_contrasts_list[0]]
+                
+                venn_filename = f"{out_path_figs}/Venn_{line}_f_vs_m.png"
+                venn_title = f"sig. DE genes overlap ({line})\nfemales and males"
+                shared_list = plot_venn_DE_genes(venn_paths_dict, venn_filename=venn_filename, venn_title=venn_title, get_shared_list=True)
+                print(f"{len(shared_list)} genes : \n{shared_list}")
 
     ############################################
     ######## MAKE ALL THE SB COMPARISONS #######
@@ -460,10 +496,6 @@ if __name__ == "__main__":
                         title = f"{category}: female - male"
                     elif "age bias" in LFC_cat:
                         title = f"{category}: day 18 - mean(day 14, day 16)"
-                        all_IDs = []
-                        for geneIDs in excl_line_bias_lists.values():
-                            all_IDs.extend(geneIDs)
-                        excl_geneIDs = list(set(all_IDs)) # use all lists since we use all time points
                     else:
                         title = LFC_cat
 
