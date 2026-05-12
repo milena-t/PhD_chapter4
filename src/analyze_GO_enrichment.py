09d837d6de06f88da8f3e2aed88503ed6702bad2
milena-t <milena.trabert@t-online.de>
2026-05-12 17:59:41 +0200

    GO enrichment lists

--- src/analyze_GO_enrichment.py
+++ src/analyze_GO_enrichment.py
@@ -5,6 +5,7 @@ Analyze the significantly enriched GO terms in all the contrasts
 import pandas as pd
 import upsetplot
 import matplotlib.pyplot as plt
+import go3
 
 
 def get_tables(username="miltr339"):
@@ -75,6 +76,67 @@ def get_tables(username="miltr339"):
 
     return out_dict
 
+    
+
+
+def get_interesting_GO_overlap_lists():
+    """
+    list of contrasts whose intersection of enriched GO terms is interesting
+    """    
+    out_dict = {
+        "sex_separated" : {
+            "females" :{
+                "day14" : ["SL1_14 - SL3_14"],
+                "day16" : ["SL1_16 - SL3_16"],
+                "day18" : ["SL1_18 - SL3_18"],
+                "SL3_day18-14" : ["SL3_18 - SL3_14"],
+            },
+            "males" : {
+                "day14" : ["SL1_14 - SL3_14"],
+                "day16" : ["SL1_16 - SL3_16"],
+                "day18" : ["SL1_18 - SL3_18"],
+                "SL1_day18-14" : ["SL1_18 - SL1_14"],
+                "SL1_day18-16" : ["SL1_18 - SL1_16"],
+                "SL1_day14-16" : ["SL1_14 - SL1_16"],
+                "SL3_day18-14" : ["SL3_18 - SL3_14"],
+                "SL3_day18-16" : ["SL3_18 - SL3_16"],
+                "SL3_day14-16" : ["SL3_14 - SL3_16"],
+            }
+        },
+        "line_separated" : {
+            "SL1" : {
+                "all" : ["F_14 - M_14","F_16 - M_16", "F_18 - M_18"],
+                "early" : ["F_14 - M_14","F_16 - M_16"],
+                "late" : ["F_16 - M_16", "F_18 - M_18"]
+            },
+            "SL3" : {
+                "all" : ["F_14 - M_14","F_16 - M_16", "F_18 - M_18"],
+                "early" : ["F_14 - M_14","F_16 - M_16"],
+                "late" : ["F_16 - M_16", "F_18 - M_18"]
+            }
+        },
+        "day_separated" : {
+            "day14" : {
+                "SL1" : ["F_1 - M_1"],
+                "SL3" : ["F_3 - M_3"],
+                "both" : ["F_1 - M_1","F_3 - M_3"]
+            },
+            "day16" : {
+                "SL1" : ["F_1 - M_1"],
+                "SL3" : ["F_3 - M_3"],
+                "both" : ["F_1 - M_1","F_3 - M_3"]
+            },
+            "day18" : {
+                "SL1" : ["F_1 - M_1"],
+                "SL3" : ["F_3 - M_3"],
+                "both" : ["F_1 - M_1","F_3 - M_3"]
+            },
+        }
+    }
+
+    return out_dict
+
+
 
 def read_R_GO_table(table_filepath:str, p_val = 0.05):
     """
@@ -88,31 +150,66 @@ def read_R_GO_table(table_filepath:str, p_val = 0.05):
     return GO_list
 
 
-def plot_enriched_GO_overlap(GO_Terms_dict:dict, plot_filename:str, plot_title  = "", min_overlap = 0):
+def plot_enriched_GO_overlap(GO_Terms_dict:dict, plot_filename:str, plot_title  = "", min_overlap = 0, contrasts_of_interest = {}):
     """
     make an upseetplot of sig DE genes within each data separation
     """
     data = upsetplot.from_contents(GO_Terms_dict)
-    if min_overlap>0:
-        upsetplot.UpSet(data, subset_size="count", show_counts=True, min_subset_size=min_overlap, sort_by="cardinality", sort_categories_by="input").plot()
-    else:
-        upsetplot.UpSet(data, subset_size="count", sort_by="cardinality", show_counts=True, min_subset_size=min_overlap).plot()
 
-    if min_overlap >0:
-        plt.subplots_adjust(top=0.85)    
-    plt.title(plot_title)
-    plt.tight_layout()
-    plt.savefig(plot_filename, dpi = 300, transparent = True)
-    print(f"plot saved in current working directory as: {plot_filename}")
-    plt.clf()
-    plt.cla()
-    plt.close()
+    if len(contrasts_of_interest) == 0:
+        if min_overlap>0:
+            upsetplot.UpSet(data, subset_size="count", show_counts=True, min_subset_size=min_overlap, sort_by="cardinality", sort_categories_by="input").plot()
+        else:
+            upsetplot.UpSet(data, subset_size="count", sort_by="cardinality", show_counts=True, min_subset_size=min_overlap).plot()
+
+        if min_overlap >0:
+            plt.subplots_adjust(top=0.85)    
+        plt.title(plot_title)
+        plt.tight_layout()
+        plt.savefig(plot_filename, dpi = 300, transparent = True)
+        print(f"plot saved in current working directory as: {plot_filename}")
+        plt.clf()
+        plt.cla()
+        plt.close()
+    else:
+        # make a table that gives a GO term list of all the contrasts of interest as they appear in the upsetplot
+        table_filename = plot_filename.replace(".png", ".txt")
+        membership_cols = data.index.names
+        df = data.reset_index()
+        with open(table_filename, "w") as table_out:
+            for subset_name , contrasts_list in contrasts_of_interest.items():
+
+                ## get intersection GO list
+                intersection = df[df[contrasts_list].all(axis=1) & (df[membership_cols].sum(axis=1) == len(contrasts_list))] # all contrasts_list are true & sum of all is the same length as contrasts_list (therefore all others must be false)
+                intersection = intersection["id"].tolist()
+
+                ## write to table
+                print(f"\t - {subset_name} : {len(intersection)} GO:terms")
+                intersection_string = ",".join(intersection)
+                contrasts_string = " AND ".join(contrasts_list)
+                outfile_line = f"{subset_name}\t{contrasts_string}\t{intersection_string}\n"
+                table_out.write(outfile_line)
+        print(f"GO:terms of specified intersections saved as: {table_filename}")
+
 
 
 if __name__ == "__main__":
     username = "miltr339"
     go_tables_paths = get_tables(username=username)
+    contrasts_of_interest = get_interesting_GO_overlap_lists()
     out_path_figs = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/DE_figures_python/GO_enrichment"
+    
+    ## don't plot only get lists of all the GO terms in each interaction
+    plot = False
+
+    ### TODO analyze go enrichment
+    # either: REVIGO online tool: http://revigo.irb.hr/
+    # or: GO3 library: https://go3.readthedocs.io/en/latest/examples.html
+    #   * requires a GAF file https://geneontology.org/docs/go-annotation-file-gaf-format-2.2/ unsure if i have that
+    #   * unsure what to do now
+    # if plot==False:
+    #     go3.load_go_terms("")
+    #     annots = go3.load_gaf(f"/Users/{username}/work/c_maculatus/C_mac_eggnog_diamond.emapper.annotations_geneIDs")
 
     if True:
         for separation, seps_dict in go_tables_paths.items():
@@ -137,12 +234,21 @@ if __name__ == "__main__":
                     plot_title = f"{separation_}:{category}\nsig. GO:terms overlap\n(min. overlap size: {min_overlap})"
                 else:
                     plot_title = f"{separation_}:{category}\nsig. GO:terms overlap"
-                plot_enriched_GO_overlap(sig_GO_terms, plot_filename= f"{out_path_figs}/upsetplot_GO_terms_{separation}_{category}.png", plot_title = plot_title, min_overlap = min_overlap)
+                
+                if plot:
+                    ## plot intersection sizes
+                    plot_enriched_GO_overlap(sig_GO_terms, plot_filename= f"{out_path_figs}/upsetplot_GO_terms_{separation}_{category}.png", plot_title = plot_title, min_overlap = min_overlap, plot=plot)
+                else:
+                    ## do semantic clustering on contrasts of interest
+                    contrasts_dict = contrasts_of_interest[separation][category]
+                    plot_enriched_GO_overlap(sig_GO_terms, plot_filename= f"{out_path_figs}/upsetplot_GO_terms_{separation}_{category}.png", plot_title = plot_title, min_overlap = min_overlap, contrasts_of_interest=contrasts_dict)
 
-            min_overlap = 5
-            if min_overlap>0:
-                plot_title = f"{separation_}:all categories\nsig. GO:terms overlap\n(min. overlap size: {min_overlap})"
-            else:
-                plot_title = f"{separation_}:all categories\nsig. GO:terms overlap"
-            plot_enriched_GO_overlap(sep_GO_terms, plot_filename= f"{out_path_figs}/upsetplot_GO_terms_{separation}_all.png", plot_title = plot_title, min_overlap = min_overlap)
+            if plot:
+                min_overlap = 5
+                if min_overlap>0:
+                    plot_title = f"{separation_}:all categories\nsig. GO:terms overlap\n(min. overlap size: {min_overlap})"
+                else:
+                    plot_title = f"{separation_}:all categories\nsig. GO:terms overlap"
+                plot_enriched_GO_overlap(sep_GO_terms, plot_filename= f"{out_path_figs}/upsetplot_GO_terms_{separation}_all.png", plot_title = plot_title, min_overlap = min_overlap, plot=plot)
+        
             
