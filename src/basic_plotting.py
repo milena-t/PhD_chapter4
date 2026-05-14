49a554bc3335759c57648f5128515e83a5d1669e
milena-t <milena.trabert@t-online.de>
2026-05-14 20:16:31 +0200

    update plots

--- src/basic_plotting.py
+++ src/basic_plotting.py
@@ -126,16 +126,16 @@ def plot_counts(counts_table:str, geneIDs_list, outfile_name:str, y_label="norma
     ax.tick_params(axis='y', labelsize=fs)
     ax.set_ylabel(f"{y_label}", fontsize = fs)
     plt.tight_layout()
-    plt.savefig(outfile_name, dpi = 300, transparent = False)
+    plt.savefig(outfile_name, dpi = 300, transparent = True)
     print(f"plot saved in current working directory as: {outfile_name}")
 
 
 if __name__ == "__main__":
     
-    username = "miltr339"
-    counts_file = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/gene_counts_normalized_nolog.tsv"
+    username = "milena"
 
-    if False:
+    if True:
+        counts_file = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/gene_counts_normalized_nolog.tsv"
         yTor_IDs = {"yTor-all":"yTor","gene-30110":"aTor"}
         yTor_plot = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/yTor_analysis/merged_yTor_aTor_counts.png"
         plot_counts(counts_table=counts_file, geneIDs_list=yTor_IDs, outfile_name=yTor_plot, remove_females=True)
@@ -144,7 +144,8 @@ if __name__ == "__main__":
         yTor_plot = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/yTor_analysis/merged_yTor_counts.png"
         plot_counts(counts_table=counts_file, geneIDs_list=yTor_IDs, outfile_name=yTor_plot, remove_females=True)
         if False:
-            ## split yTor lists
+            ## split yTor lists -> HAS TO USE OLD FILE WITH ACTUAL SPLIT GENEIDS!
+            counts_file = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/gene_counts_normalized_nolog.tsv"
             # all TOR
             yTor_IDs = ["yTor-A", "yTor-B", "yTor-C","gene-30110"]
             yTor_plot = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/yTor_analysis/all_Tor_counts.png"
@@ -158,7 +159,8 @@ if __name__ == "__main__":
         MSL2_plot = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/yTor_analysis/MSL2_counts.png"
         plot_counts(counts_table=counts_file, geneIDs_list=MSL2_IDs, outfile_name=MSL2_plot)
 
-    if False:
+    if True:
+        counts_file = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/gene_counts_normalized_nolog.tsv"
         # all Y expressed
         y_expr = ["gene-371805","gene-371844","gene-371889","gene-371913","gene-371922","gene-371957","gene-372053","gene-372068","gene-372216","gene-372264","yTor-all"]
         y_plot = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/yTor_analysis/y_expr_counts.png"
