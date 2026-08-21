15f1ae939d3a9cde673135288bbbe9c237d86c71
milena-t <milena.trabert@t-online.de>
2026-08-21 16:49:36 +0200

    sex bias F mean line 2

--- src/plot_DE_stuff.py
+++ src/plot_DE_stuff.py
@@ -1012,14 +1012,14 @@ if __name__ == "__main__":
     
     # only one of the below ones can be true at the same time! if both are false, smear/volcano plots are created by default
     ############
-    make_upset = True # don't plot the smear/volcano plots but insetad make category-wise upset plots of DE genes
+    make_upset = False # don't plot the smear/volcano plots but insetad make category-wise upset plots of DE genes
     ############
-    make_list_outfiles = True # don't plot anything, instead make output files with lists of significant geneIDs for each contrast
+    make_list_outfiles = False # don't plot anything, instead make output files with lists of significant geneIDs for each contrast
     lists_outdir = f"/Users/{username}/work/PhD_code/PhD_chapter4/data/sig_DE_genes_lists"
     ############
     highlight_yTOR = False
 
-    if True:
+    if False:
         
         if make_upset or make_list_outfiles:
             plot=False
@@ -1317,7 +1317,7 @@ if __name__ == "__main__":
     ############################################
     
     ## standard sets matching the tabs in the html
-    if False:
+    if True:
         venn_sets = {
             "no_separation" : { "all_samples" : {"" : []}},
             "sex_separated" : {
@@ -1350,6 +1350,9 @@ if __name__ == "__main__":
             }
         }
         for separation, seps_dict in table_paths.items():
+            if separation != "day_separated":
+                continue
+
             print(f"\n=========================== {separation} ===========================")
 
             for category, paths_dict in seps_dict.items():
