628686e403d04db6787d17260fc16358ba20166a
milena-t <milena.trabert@t-online.de>
2026-08-24 11:55:12 +0200

    reset to orig within line sex bias

--- src/plot_DE_stuff.py
+++ src/plot_DE_stuff.py
@@ -1032,7 +1032,7 @@ if __name__ == "__main__":
     ############
     highlight_yTOR = False
 
-    if True:
+    if False:
         
         if make_upset or make_list_outfiles:
             plot=False
